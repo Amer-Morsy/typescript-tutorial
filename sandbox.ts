c4c63bf702d7dc1ee727524782bfb43b0dee55d7
Amer-Morsy <amermousa341@gmail.com>
2025-03-22 14:06:05 +0300

    lesson 4

--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,43 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['Alice', 'Bob', 'Charlie'];
+// names ='Alice'; // Error
 
-character = 'luigi'; // valid
-// character = 20; // invalid
+names.push('Dave'); // OK
+// names.push(123); // Error
+names[2] = 'Eve'; // OK
+// names[2] = 456; // Error
 
-age = 40; // valid
-// age = 'yoshi'; // invalid
+let numbers = [1, 2, 3];
 
-isBlackBelt = true  // valid
-// isBlackBelt = 1; // invalid
+numbers.push(4); // OK
+// numbers.push('Four'); // Error
+numbers[2] = 5; // OK
+// numbers[2] = 'Five'; // Error
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-}
+let mixed = ['Alice', 1, 'Bob', 2];
 
-console.log(circ(7.5)); // valid
-// console.log(circ('7.5')); // invalid
+mixed.push('Charlie'); // OK
+mixed.push(5); // OK
+// mixed.push(true); // Error
+mixed[2] = 'Dave'; // OK
+// mixed[2] = false; // Error
+
+// objects
+let ninja = {
+  name: 'Yoshi',
+  age: 30,
+  belt: 'black'
+};
+//  ninja = true // Error
+
+ninja.age = 40; // OK
+// ninja.age = 'forty'; // Error
+// ninja.skills = ['eating', 'sneaking']; // Error
+
+ninja = {
+  name: 'Yoshi',
+  age: 40,
+  belt: 'orange',
+  // skills: ['eating', 'sneaking'] // invalid
+};
+
