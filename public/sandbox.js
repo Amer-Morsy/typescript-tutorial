cb82dcde761db3c5f378d0045643a5ff571cfe84
Amer-Morsy <amermousa341@gmail.com>
2025-03-22 18:10:01 +0300

    lesson 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('Hello, world!');
+let greet = () => {
+    console.log('Hello, World!');
+};
+greet(); // Hello, World!
+// greet = 'Hello, World!'; // Error: Type 'string' is not assignable to type 'Function'.
+greet = () => {
+    console.log('Hello, Again!');
+};
+greet(); // Hello, Again!
+const add = (a, b, c /*?*/ = 0) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10); // 15, 0
+add(5, 10, '20'); // 15, 20
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+console.log(result); // 3
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,27 @@
-console.log('Hello, world!');
+let greet: Function = () => {
+  console.log('Hello, World!');
+};
+
+greet(); // Hello, World!
+// greet = 'Hello, World!'; // Error: Type 'string' is not assignable to type 'Function'.
+
+greet = () => {
+  console.log('Hello, Again!');
+}
+
+greet(); // Hello, Again!
+
+const add = (a: number, b: number, c/*?*/: number | string = 0): void => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add(5, 10); // 15, 0
+add(5, 10, '20'); // 15, 20
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+}
+
+let result = minus(10, 7);
+console.log(result); // 3
