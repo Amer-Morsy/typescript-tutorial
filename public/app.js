55d46ee24aac95a495fa2bd6a505e52e49a04c5b
Amer-Morsy <amermousa341@gmail.com>
2025-03-24 01:16:45 +0300

    lesson-12

--- public/app.js
+++ public/app.js
@@ -1,15 +1,20 @@
 "use strict";
-const anchor = document.createElement('a');
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
-const form = document.querySelector('form');
-// console.log(form.children);
-// inputs 
-const type = document.querySelector('#type');
-const toFrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
-const anchor = document.createElement('a')!;
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
 
-// console.log(anchor.href);
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
 
-// const form = document.querySelector('form')!;
-const form = document.querySelector('form') as HTMLFormElement;
-// console.log(form.children);
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
 
-// inputs 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+ 
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value,
-    toFrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
-}); 
+invoices.forEach(inv => {
+  console.log(inv.client, inv.amount, inv.format());
+});
